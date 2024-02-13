1d25953ae98b4698f67ecae57848671dcd17db66
justbenuk <justbenuk@gmail.com>
2024-02-13 06:32:43 +0000

    initial committ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,8 @@
-import { Button } from "@nextui-org/react";
-import { auth } from "@/auth";
-import Profile from "@/components/profile";
-import { SignIn, SignOut } from "@/actions";
-export default async function Home() {
-  const session = await auth()
+export default function Home() {
 
   return (
     <div>
-      <form action={SignIn}>
-        <Button type='submit'>Sign In</Button>
-      </form>
-      <form action={SignOut}>
-        <Button type='submit'>Sign Out</Button>
-      </form>
-
-      {session?.user ? <div>{JSON.stringify(session.user)}</div> : <div>signed out</div>}
-      <Profile />
+      home page
     </div>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,45 @@
 import Link from "next/link"
-import { Navbar, NavbarBrand, NavbarItem, NavbarContent, Input, Button, Avatar } from "@nextui-org/react"
+import { Navbar, NavbarBrand, NavbarItem, NavbarContent, Input, Button, Avatar, Popover, PopoverTrigger, PopoverContent } from "@nextui-org/react"
 import { auth } from "@/auth"
+import { ReactNode } from "react"
+import * as actions from '@/actions'
+
 export default async function Header() {
   //check if a user is signed in
   const session = await auth()
+
+  //if user is logged in
+  let authContent: ReactNode
+  if (session?.user) {
+    authContent = <>
+      <Popover placement='left'>
+        <PopoverTrigger>
+          <Avatar src={session.user.image || ''} />
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="p-4">
+            <form action={actions.SignOut}>
+              <Button type='submit'>Sign Out</Button>
+            </form>
+          </div>
+        </PopoverContent>
+      </Popover>
+    </>
+  } else {
+    authContent = <>
+      <NavbarItem>
+        <form action={actions.SignIn}>
+          <Button type='submit' color='secondary' variant='bordered'>Sign In</Button>
+        </form>
+      </NavbarItem>
+      <NavbarItem>
+        <form action={actions.SignIn}>
+          <Button type='submit' color='primary' variant='flat'>Sign Up</Button>
+        </form>
+      </NavbarItem>
+    </>
+  }
+
   return (
     <Navbar className="shadow mb-6">
       <NavbarBrand>
@@ -14,15 +50,8 @@ export default async function Header() {
           <Input />
         </NavbarItem>
       </NavbarContent>
-
       <NavbarContent justify='end'>
-        <NavbarItem>
-          {session?.user ? (
-            <p>profile</p>
-          ) : (
-            <p>login</p>
-          )}
-        </NavbarItem>
+        {authContent}
       </NavbarContent>
     </Navbar>
   )
